7ef6f08ee00cad9118395eb30c9761050f38a272
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-11-26 21:10:57 +0000

    Update all names.

--- tinycdxserver-bootstrap.sh
+++ tinycdxserver-bootstrap.sh
@@ -2,8 +2,8 @@ export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 
 git clone https://github.com/nla/outbackcdx.git
 git checkout 0.3.2
-cd tinycdxserver/
+cd outbackcdx/
 
 mvn package || exit
 
-cp target/tiny*.jar tinycdxserver.jar
+cp target/outbackcdx*.jar outbackcdx.jar
