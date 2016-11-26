0c2a557cefac59600ae808334d3e19ff33faf954
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-11-26 19:24:40 +0000

    Switch to v.0.3.2 from official repo.

--- tinycdxserver-bootstrap.sh
+++ tinycdxserver-bootstrap.sh
@@ -1,6 +1,7 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 
-git clone https://github.com/anjackson/tinycdxserver.git
+git clone https://github.com/nla/outbackcdx.git
+git checkout 0.3.2
 cd tinycdxserver/
 
 mvn package || exit
