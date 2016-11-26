947943ca41f3aeceb0b4cd892a78eee0d1c76776
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-11-26 23:12:51 +0000

    Resolve certificates issue hampering downloads.

--- tinycdxserver-bootstrap.sh
+++ tinycdxserver-bootstrap.sh
@@ -1,9 +1,12 @@
+apt-get install -y ca-certificates-java
+#dpkg-reconfigure ca-certificates-java
+
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 
 git clone https://github.com/nla/outbackcdx.git
+cd outbackcdx
 git checkout 0.3.2
-cd outbackcdx/
 
 mvn package || exit
 
-cp target/outbackcdx*.jar outbackcdx.jar
+cp target/tiny*.jar tinycdxserver.jar
